0379bf6f869d5f4beacf5b5a912009f02dbe9acd
ark0f <ark0f@users.noreply.github.com>
2024-08-06 01:16:38 +0000

    [create-pull-request] automated change

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $toolsDir   = Join-Path $(Get-ToolsLocation) $env:ChocolateyPackageName
 $pp = Get-PackageParameters
 $ryujinx_folder = "$toolsDir\publish"
-$url64      = 'https://github.com/Ryujinx/release-channel-master/releases/download/1.1.537/ryujinx-1.1.537-win_x64.zip'
-$checksum64 = 'f346a062085e87a2eb388ba3593c325c11e496163b546a377e0f7ef472c1a47d'
+$url64      = 'https://github.com/Ryujinx/release-channel-master/releases/download/1.1.1372/ryujinx-1.1.1372-win_x64.zip'
+$checksum64 = 'f1f96c87309c6741ad9cafbfaa6e9d7104b7f2f6f510a6a7b9070796ba16cb20'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
